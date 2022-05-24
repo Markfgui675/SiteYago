5064127f26bacd75faab57f6a1a9ca2b15b25383
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2022-05-24 18:43:17 -0300

    Docs and prog
    Documentos, imagens e arquivos do site.

---
+++ carrossel.js
@@ -0,0 +1,88 @@
+let slider = tns({
+    container : ".my-slider",
+    "slideBy" : "1",
+    "speed" : 400,
+    "nav" : false,
+    "mouseDrag" : true,
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    responsive: {
+        1600: {
+            items: 4,
+            gutter : 20
+        },
+        1024: {
+            items: 3,
+            gutter: 20            
+        },
+        768: {
+            items: 2,
+            gutter: 20
+        },
+        480: {
+            items: 1,
+            gutter: 20
+        }
+    }
+})
+
+
+let slider_2 = tns({
+    container : ".my-slider_2",
+    "slideBy" : "1",
+    "speed" : 400,
+    "nav" : false,
+    "mouseDrag" : true,
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    responsive: {
+        1600: {
+            items: 4,
+            gutter : 20
+        },
+        1024: {
+            items: 3,
+            gutter: 20            
+        },
+        768: {
+            items: 2,
+            gutter: 20
+        },
+        480: {
+            items: 1,
+            gutter: 20
+        }
+    }
+})
+
+
+let slider_3 = tns({
+    container : ".my-slider_3",
+    "slideBy" : "1",
+    "speed" : 400,
+    "nav" : false,
+    "mouseDrag" : true,
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    responsive: {
+        1600: {
+            items: 4,
+            gutter : 20
+        },
+        1024: {
+            items: 3,
+            gutter: 20            
+        },
+        768: {
+            items: 2,
+            gutter: 20
+        },
+        480: {
+            items: 1,
+            gutter: 20
+        }
+    }
+})
